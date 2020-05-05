66500a4fbb371f5cc7d0d4cd7dc9ca99e543c020
Ruohan Yan <yrh@berkeley.edu>
2020-05-05 22:03:01 +0800

    Term selection features

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let api = "http://118.25.79.158:3000/api/v1/";
 
 $(() => {
     $("#search-input").on("input", () => {
-        filter($("#search-input").val());
+        filter();
     });
     $(".toggle-button").on("click", () => {
         $("#about-container").toggleClass("hidden");
@@ -33,13 +33,14 @@ $(() => {
 
 let ids=[];
 
-function addCard(id, name, url) {
+function addCard(id, name, url, term) {
     ids.push(id);
     let lastSpace = id.lastIndexOf(" ");
     let codePart = lastSpace == -1 ? "" : id.substring(0, lastSpace);
     let numPart = id.substring(lastSpace + 1, id.length);
     let newCard = $(
-        `<div class="card" data-id="${id}" data-name="${name}" data-url="${url}">
+        `<div class="card" data-id="${id}" data-name="${name}"
+                           data-url="${url}" data-term="${term}">
             <div class="id-wrapper">
                 <div class="id">
                     <span>
@@ -65,12 +66,14 @@ function cardClick(e) {
     window.location.href = $(card).data("url");
 }
 
-function filter(substring) {
-    substring = substring.toLowerCase()
+function filter() {
+    substring = $("#search-input").val().toLowerCase()
+    let term = $("input[type='radio']:checked").data("term");
     for (let id of ids) {
         let card = $(`.card[data-id="${id}"]`);
         if ((id.toLowerCase().indexOf(substring) == -1) &&
-            (card.data("name").toLowerCase().indexOf(substring)) == -1) {
+                ((card.data("name").toLowerCase().indexOf(substring)) == -1) ||
+                (term !== card.data("term"))) {
             card.addClass("hidden");
         } else {
             card.removeClass("hidden");
@@ -98,9 +101,55 @@ function loadCourses(token) {
     }, success: (response) => {
         $("#card-container").html("");
         $("#main-container").addClass("loaded");
+        let allTerms = new Set();
         for (let course of response) {
-            addCard(course.code, course.name, course.qr_code);
+            addCard(course.code, course.name, course.qr_code, course.term);
+            allTerms.add(course.term);
         }
+        let termsArray = [];
+        for (let x of allTerms) {
+            termsArray.push(x);
+        }
+        let termToInt = (x) => {
+            let separator = x.indexOf(" ");
+            if (separator == -1) {
+                return -1;
+            } else {
+                let season = x.substring(0, separator);
+                let year = x.substring(separator + 1, x.length);
+                let seasonInt;
+                switch (season.toLowerCase()) {
+                    case "spring":
+                        seasonInt = 0;
+                        break;
+                    case "summer":
+                        seasonInt = 1;
+                        break;
+                    case "fall":
+                        seasonInt = 2;
+                        break;
+                    default:
+                        return -1;
+                }
+                return year * 3 + seasonInt;
+            }
+        };
+        termsArray.sort((a, b) => {
+            return termToInt(b) - termToInt(a);
+        });
+        for (let term of termsArray) {
+            let termId = term.replaceAll(" ", "-");
+            let radio = $(`
+                <input type="radio" name="term" id="term-${termId}" data-term="${term}" />
+                <label for="term-${termId}">${term}</label>
+            `);
+            $("#term-container").append(radio);
+            $(radio[0]).on("change", (e) => {
+                filter();
+            });
+        }
+        $(`#term-${termsArray[0].replaceAll(" ", "-")}`).attr("checked", "checked");
+        filter();
     }, error: (response) => {
         console.log(response);
     }});
