5a7b309fba63511304f29fd972c12286f2b5d0e5
Yuanhan Li <williamlyh@berkeley.edu>
2020-06-06 18:00:27 +0800

    Restructure add.js & updates to Add
    - Restructure add.js for better code readability
    - Inform submit status
    - freeze submit button while submitting

--- scripts/add.js
+++ scripts/add.js
@@ -1,22 +1,15 @@
+let serverURL = 'https://uncertaintycc.com/api/v1/';
 let token = "";
 let courseURL = "";
-let helpLoaded = false;
+let courseTerm = "";
+let courseCode = "";
+let courseName = "";
 
 $(() => {
     token = readCookie("token");
     if (!token) {
         window.location.href = 'index.html?redirect=add';
     }
-    $("#help-button").on("click", () => {
-        if (!helpLoaded) {
-            $("#help-page-container").load("add-help.html");
-            helpLoaded = true;
-        }
-        $("#help-container").toggleClass("hidden");
-    });
-    $("#help-close-button").on("click", () => {
-        $("#help-container").toggleClass("hidden");
-    });
 });
 
 function readCookie(name) {
@@ -28,43 +21,6 @@ function readCookie(name) {
         }
     }
     return null;
-}
-
-function processSubmit() {
-    if (isLegalCourse()) {
-        let term = $("input[name='term']:checked").val();
-        let depCode = $("#dep-code").val().toUpperCase();
-        let courseCode = $("#course-code").val().toUpperCase();
-        findDuplicate(term, depCode, courseCode, courseURL);
-    } else {
-        $("#submit-text").text("课程信息不正确");
-    }
-}
-
-function submitInfo(name, code, term) {
-    $.ajax({
-        type: 'POST',
-        url: 'https://uncertaintycc.com/api/v1/tickets/',
-        headers: {
-            "Authorization": 'Bearer ' + token,
-            "Content-Type": 'application/json',
-        },
-        dataType: 'json',
-        data: JSON.stringify({
-            "name": name,
-            "code": code,
-            "term": term,
-            "qr_code": courseURL,
-        }),
-        success: (response) => {
-            console.log(response);
-            $("#submit-text").text("添加成功！审核通过后二维码就会出现啦");
-        },
-        error: (response) => {
-            console.log(response);
-            $("#submit-text").text("添加失败，请稍后重试");
-        },
-    });
 }
 
 function loadPreview() {
@@ -98,7 +54,7 @@ function loadPreview() {
                 updatePageURLWithImageUploaded(img_data);
                 canv.style.visibility = 'visible';
             } catch (err) {
-                $("#upload-text").text("唔，出错了，请重试");
+                $("#upload-text").text("出错了，请重试");
             }
         }
     };
@@ -113,78 +69,219 @@ function fitImageOntoCanvasAndDisplay(ctx, canv, image, width, height) {
 }
 
 function updatePageURLWithImageUploaded(image_data){
-    let url = getURL(image_data);
-    $("#upload-text").text("");
-    switch (url){
-    case 2:
-        $("#upload-text").text("好像不是二维码");
-        break;
-    case 1:
-        $("#upload-text").text("请上传微信二维码");
-        break;
-    default:
-        courseURL = url;
-        break;
+    let URLCheckResult = processURL(image_data);
+    qrAlert("正在检查二维码");
+    switch (URLCheckResult){
+        case "CORRECT_URL":
+            qrAlert("");
+            break;
+        case "NO_UR":
+            qrAlert("好像不是微信群的二维码");
+            break;
+        case "ILLEGAL_URL":
+            qrAlert("好像不是微信群的二维码");
+            break;
+        default:
+            qrAlert("出错了，请稍后重试");
+            break;
     }
 }
 
 function isLegalURL(url) {
     let regex = new RegExp("^https://weixin.qq.com/g/([a-zA-Z0-9-_]+)$");
-    if (regex.test(url)) {
-        return true;
-    }
-    return false;
+    return regex.test(url);
 }
 
-function getURL(image_data) {
+function processURL(image_data) {
     let qrCodeRead = jsQR(image_data.data, image_data.width, image_data.height);
+    let url;
     if (qrCodeRead) {
         url = qrCodeRead.data;
-        if (isLegalURL(url)){
-            return url;
+        if (isLegalURL(url)) {
+            courseURL = url;
+            return "CORRECT_URL";
+        } else {
+            return "ILLEGAL_URL";
         }
-        return 1;
-    }
-    else {
-        return 2;
+    } else {
+        return "NO_URL";
     }
 }
 
-function getCode() {
-    return $("#dep-code").val().toUpperCase() + " " + $("#course-code").val().toUpperCase();
+function qrAlert(message) {
+    $("#upload-text").text(message);
 }
 
-function findDuplicate(term, depCode, couCode, url) {
+function startSubmit() {
+    freezeSubmitButton();
+    submitAlert("让我们先检查一下格式");
+    if (!processInput()) {
+        restoreSubmitButton();
+    } else {
+        checkDuplicateAndSubmit();
+    }
+}
+
+function freezeSubmitButton() {
+    document.getElementById("submit-button").onclick = function () {};
+}
+
+function restoreSubmitButton() {
+    document.getElementById("submit-button").onclick = function () {startSubmit();};
+}
+
+function processInput() {
+    return processCourseInput() && processQRInput();
+}
+
+function processCourseInput() {
+    let courseTermInput = processCourseTermInput();
+    if (!courseTermInput) {
+        return false;
+    }
+    let depCodeInput = processDepCodeInput();
+    if(!depCodeInput) {
+        return false;
+    }
+    let numCodeInput = processNumCodeInput();
+    if(!numCodeInput) {
+        return false;
+    }
+    let courseNameInput = processCourseNameInput();
+    if(!courseNameInput) {
+        return false;
+    }
+    courseTerm = courseTermInput;
+    courseCode = depCodeInput + " " + numCodeInput;
+    courseName = courseNameInput;
+    return true;
+}
+
+function processCourseTermInput() {
+    let courseTermInput = $("input[name='term']:checked").val();
+    if (!courseTermInput) {
+        submitAlert("挑一个学期呗");
+        return null;
+    } else {
+        return courseTermInput;
+    }
+}
+
+function processDepCodeInput() {
+    let depCodeInput = $("#dep-code").val();
+    if(checkNonemptyAndLegal("DEP_CODE", depCodeInput)){
+        return reduceDepCodeVariation(depCodeInput.toUpperCase());
+    } else {
+        return null;
+    }
+}
+
+function processNumCodeInput() {
+    let numCodeInput = $("#num-code").val();
+    if(checkNonemptyAndLegal("NUM_CODE", numCodeInput)) {
+        return numCodeInput.toUpperCase();
+    } else {
+        return null;
+    }
+}
+
+function processCourseNameInput() {
+    let courseNameInput = $("#course-name").val();
+    if(checkNonemptyAndLegal("COURSE_NAME", courseNameInput)) {
+        return courseNameInput;
+    } else {
+        return null;
+    }
+}
+
+function isEmpty(value) {
+    if (!value) {
+        return true;
+    } else return value.replace(/(^s*)|(s*$)/g, "").length === 0;
+}
+
+function isIllegal(FLAG, value) {
+    switch (FLAG) {
+        case "DEP_CODE":
+            let depCodeReg = new RegExp("^[a-zA-Z]+$");
+            return !depCodeReg.test(value);
+        case "NUM_CODE":
+            let numCodeReg = new RegExp("^[a-zA-Z]?[0-9]+[a-zA-Z]*(-[0-9]{3})?$");
+            return !numCodeReg.test(value);
+        case "COURSE_NAME":
+            let courseNameReg = new RegExp("^[a-zA-Z0-9()_ ]+$");
+            return !courseNameReg.test(value) && value.length < 51;
+    }
+}
+
+function checkNonemptyAndLegal(FLAG, value) {
+    if (isEmpty(value)) {
+        submitAlert("课程信息没有填全哦");
+        return false;
+    } else if (isIllegal(FLAG, value)) {
+        submitAlert("课程信息格式不正确");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function reduceDepCodeVariation(depCode) {
+    switch (depCode) {
+        case "CS":
+            return "COMPSCI";
+        case "NST":
+            return "NUSCTX";
+        case "ENG":
+            return "ENGLISH";
+        default:
+            return depCode;
+    }
+}
+
+function processQRInput() {
+    if (isLegalURL(courseURL)) {
+        return true;
+    } else {
+        submitAlert("还没上传正确的二维码哦");
+        return false;
+    }
+}
+
+
+function checkDuplicateAndSubmit() {
+    submitAlert("正在加载已有的二维码，稍等");
     $.ajax({
-        url: 'https://uncertaintycc.com/api/v1/courses/',
+        url: serverURL + 'courses/',
         headers: {
             "Authorization": "Bearer " + token,
         },
         success: (response) => {
-            let found = findInResponse(response, term, depCode, couCode, url);
+            submitAlert("正在看看有没有重复的");
+            let found = findInResponse(response, courseTerm, courseCode, courseURL);
             if (found) {
-                $("#submit-text").text("已经有了哦");
+                submitAlert("这个二维码已经存在啦");
+                restoreSubmitButton();
+                return false;
             } else {
-                submitInfo($("#course-name").val(), getCode(), term);
+                submitInfo();
             }
         },
         error: (response) => {
-            $("#submit-text").text("糟糕，服务器走丢了！");
+            console.log(response);
+            submitAlert("糟糕，服务器走丢了！");
+            restoreSubmitButton();
         }
     });
 }
 
-function findInResponse(response, term, depCode, couCode, url) {
+function findInResponse(response) {
     let found = false;
     for (let course of response) {
-        if (course.term === term) {
-            let courseCodeSplit = course.code.split(" ");
-            if (courseCodeSplit[0] === depCode && courseCodeSplit[1] === couCode) {
-                found = true;
-                break;
-            }
-        }
-        if (course.qr_code === url) {
+        if (course.term === courseTerm && course.code === courseCode) {
+            found = true;
+            break;
+        } else if (course.qr_code === courseURL) {
             found = true;
             break;
         }
@@ -192,38 +289,36 @@ function findInResponse(response, term, depCode, couCode, url) {
     return found;
 }
 
-function isNotEmpty(value) {
-    if (!value) {
-        console.log("No input");
-        return false;
-    } else if (value.replace(/(^s*)|(s*$)/g, "").length === 0) {
-        console.log("Empty input");
-        return false;
-    }
-    return true;
+
+function submitInfo() {
+    submitAlert("一切就绪，正在上船（上传）");
+    $.ajax({
+        type: 'POST',
+        url: serverURL,
+        headers: {
+            "Authorization": 'Bearer ' + token,
+            "Content-Type": 'application/json',
+        },
+        dataType: 'json',
+        data: JSON.stringify({
+            "name": courseName,
+            "code": courseCode,
+            "term": courseTerm,
+            "qr_code": courseURL,
+        }),
+        success: (response) => {
+            console.log(response);
+            submitAlert("添加成功！审核通过后二维码就会出现啦");
+            restoreSubmitButton();
+        },
+        error: (response) => {
+            console.log(response);
+            submitAlert("添加失败，请稍后重试");
+            restoreSubmitButton();
+        },
+    });
 }
 
-function isLegalCourse() {
-    let depCodeInputField = $("#dep-code");
-    let depCodeInput = depCodeInputField.val();
-    let couCodeInputField = $("#course-code");
-    let couCodeInput = couCodeInputField.val();
-    let nameInputField = $("#course-name");
-    let nameInput = nameInputField.val();
-    if (!$("input[name='term']:checked").val()) {
-        return false;
-    }
-    if (isNotEmpty(depCodeInput) && isNotEmpty(couCodeInput) && isNotEmpty(nameInput)) {
-        if (depCodeInput === "CS") {
-            depCodeInput = "COMPSCI";
-        }
-        depCodeInput = depCodeInput.toUpperCase();
-        couCodeInput = couCodeInput.toUpperCase();
-        let code = depCodeInput + " " + couCodeInput;
-        if (isLegalURL(courseURL)) {
-            return true;
-        }
-        console.log("no image");
-    }
-    return false;
+function submitAlert(message) {
+    $("#submit-text").text(message);
 }
